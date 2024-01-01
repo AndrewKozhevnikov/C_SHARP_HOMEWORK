5e113470b5a7527a96c590883ee0bf5b2ecd0b15
Andrew Kozhevnikov <a.kozh.ekb@gmail.com>
2024-01-02 03:47:52 +0500

    Homework 2, task 3 extra

--- homework_2/2_3/Program.cs
+++ homework_2/2_3/Program.cs
@@ -5,8 +5,6 @@
 Console.Write("Введите целое число из отрезка [10, 99]: ");
 int n = int.Parse(Console.ReadLine()!);
 
-// int n = 121;
-
 if (n < 10)
     Console.WriteLine("Число не должна быть меньше 10.");
 else
