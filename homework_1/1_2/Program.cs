d8e2ec72b613ac8aafbdaa1935297aef99139e7d
Andrew Kozhevnikov <a.kozh.ekb@gmail.com>
2024-01-02 04:02:27 +0500

    Homework 1, task 2

---
+++ homework_1/1_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 2. Написать программу. На входе 3 числа. На выходе максимальное из этих чисел.
+
+Console.Write("введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.Write("введите третье число: ");
+int c = int.Parse(Console.ReadLine()!);
+
+int max = a;
+
+if (b > max)
+    max = b;
+if (c > max)
+    max = c;
+
+Console.WriteLine("максимальное число " + max);
