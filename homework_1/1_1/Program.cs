b447d6cf77634a120ea8b889f171bb0a99a5d052
Andrew Kozhevnikov <a.kozh.ekb@gmail.com>
2024-01-02 03:57:56 +0500

    Homework 1, task 1

---
+++ homework_1/1_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задаяа 1. Написать программу. На входе 2 числа. На выходе, какое число больше.
+
+Console.Write("введите первое число: ");
+int firstNumber = int.Parse(Console.ReadLine()!);
+
+Console.Write("введите второе число: ");
+int secondNumber = int.Parse(Console.ReadLine()!);
+
+if (firstNumber > secondNumber)
+    Console.WriteLine("первое число больше");
+else
+{
+    if (firstNumber < secondNumber)
+        Console.WriteLine("второе число больше");
+    else
+        Console.WriteLine("числа равны");
+}
