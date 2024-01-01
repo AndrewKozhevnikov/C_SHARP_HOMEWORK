9f81a9489e20ae89f23dbf5b5cc002caf88c11b1
Andrew Kozhevnikov <a.kozh.ekb@gmail.com>
2024-01-02 04:12:35 +0500

    Homework 1, task 4

---
+++ homework_1/1_4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 4. Написать программу. На входе число (number). 
+// На выходе все четные числа от 1 до number (включительно), 
+// после каждого числа пробел.
+
+Console.Write("введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int i = 1;
+
+while (i <= number)
+{
+    if (i % 2 == 0)
+        Console.Write(i + " ");
+    i = i + 1;
+}
