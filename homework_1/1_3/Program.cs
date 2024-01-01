da40586cbeb72c28503c85973d45592ba81a515c
Andrew Kozhevnikov <a.kozh.ekb@gmail.com>
2024-01-02 04:05:48 +0500

    Homework 1, task 3

---
+++ homework_1/1_3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 3. Написать программу. На входе число number. На выходе, является ли оно четным (делится на 2 без остатка).
+
+Console.Write("введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number % 2 == 0)
+    Console.WriteLine("четное");
+else
+    Console.WriteLine("нечетное");
