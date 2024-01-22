735eefa8b44e76d82b251ec55c295dfd1f0b53b4
Andrew Kozhevnikov <a.kozh.ekb@gmail.com>
2024-01-22 23:43:55 +0500

    Homework 4, task 3

---
+++ homework_4/4_3/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая перевернет одномерный массив
+//(первый элемент станет последним, второй - предпоследним и т.д.).
+
+void ReverseArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        int a = arr[i];
+        arr[i] = arr[arr.Length - i - 1];
+        arr[arr.Length - i - 1] = a;
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        Console.Write($"{arr[i]} ");
+}
+
+int[] array = { 1, 3, 5, 6, 7, 8, 0 };
+ReverseArray(array);
+PrintArray(array);
