b98d7d626a06f9a0b5d6b47e5384d5ac7802e9d6
Andrew Kozhevnikov <a.kozh.ekb@gmail.com>
2024-01-23 13:10:37 +0500

    Homework 3, task 2

---
+++ homework_3/3_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задайте массив из 10 целых чисел.
+// Найдите количекство четных чисел в массиве.
+
+int[] array = { 2, 5, 2, 3, 6, 3, 7, 8, 2, 4 };
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+        count = count + 1;
+}
+Console.Write(count);
