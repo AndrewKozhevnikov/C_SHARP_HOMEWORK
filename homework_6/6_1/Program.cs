54779c2146cab3bba9e64d0b76064881b906b39c
Andrew Kozhevnikov <a.kozh.ekb@gmail.com>
2024-02-01 23:08:27 +0500

    Homework 6, task 1

---
+++ homework_6/6_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте двумерный массив символов (тип char[,]).
+// Создать строку из символов этого массива.
+
+char[,] charArr = {
+    {'a', 'b', 'c'},
+    {'d', 'e', 'f'}
+};
+string result = CreateString(charArr);
+Console.WriteLine(result);
+
+string CreateString(char[,] mass)
+{
+    string buf = " ";
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            buf = buf + mass[i, j];
+        }
+    }
+    return buf;
+}
