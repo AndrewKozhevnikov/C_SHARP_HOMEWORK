9fab606ddb828d644c3312e70d7103c1955ec0e6
Andrew Kozhevnikov <a.kozh.ekb@gmail.com>
2024-02-02 02:20:54 +0500

    Homework 6, task 3

---
+++ homework_6/6_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте произвольную строку.
+// Выясните, является ли она палидромом.
+
+Console.WriteLine("Введите строку: ");
+String str = Console.ReadLine();
+char[] arr = str.ToCharArray();
+if (IsPalindrom(arr))
+    Console.WriteLine("Палиндром");
+else
+    Console.WriteLine("Не палиндром");
+
+static bool IsPalindrom(char[] word)
+{
+    for (int i = 0; i < word.Length / 2; i++)
+        if (word[i] != word[word.Length - 1 - i])
+            return false;
+    return true;
+}
