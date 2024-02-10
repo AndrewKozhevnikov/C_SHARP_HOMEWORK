f2863532df4d2d0616db9832e833f9b10f8c44b5
Andrew Kozhevnikov <a.kozh.ekb@gmail.com>
2024-02-11 01:48:46 +0500

    Homework 7, task 3

---
+++ homework_7/7_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте произвольный массив.
+// Выведите его элементы, начиная с конца. Использовать рекурсию.
+
+int[] array = { 1, 2, 5, 0, 10, 34 };
+PrintArrayReversely(array, array.Length - 1);
+
+void PrintArrayReversely(int[] array, int i)
+{
+    if (i < 0)
+        return;
+    Console.Write(array[i] + " ");
+    PrintArrayReversely(array, i - 1);
+}
