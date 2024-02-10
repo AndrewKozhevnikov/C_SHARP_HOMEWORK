55ea5a346c1dfea9bea42a1233e04f566b3f9ec0
Andrew Kozhevnikov <a.kozh.ekb@gmail.com>
2024-02-11 00:16:24 +0500

    Homework 7, task 1

---
+++ homework_7/7_1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте значение M и N. Написать программу, 
+// которая выведет все натуральные числа в промежутке от M до N.
+// Использовать рекурсию.
+
+Console.Write("Введите значение M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение N: ");
+int n = int.Parse(Console.ReadLine()!);
+if (m > n)
+    Console.Write("M должно быть меньше N");
+else
+{
+    Console.Write("Все натуральные числа от M до N: ");
+    PrintNumbers(m, n);
+}
+
+void PrintNumbers(int m, int n)
+{
+    if (m <= n)
+    {
+        Console.Write($"{m} ");
+        PrintNumbers(m + 1, n);
+    }
+}
