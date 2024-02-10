b4550873c9a123439f107adb63f877e67cb0d16d
Andrew Kozhevnikov <a.kozh.ekb@gmail.com>
2024-02-11 00:46:16 +0500

    Homework 7, task 2

---
+++ homework_7/7_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+
+Console.Write("Введите неотрицательное число m: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите неотрицательное число n: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Функция Аккермана: " + Ack(m, n));
+
+int Ack(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else
+        if (n == 0)
+        return Ack(m - 1, 1);
+    else
+        return Ack(m - 1, Ack(m, n - 1));
+}
